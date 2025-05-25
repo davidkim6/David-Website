8a9954fb21afb2a1e06747f93e3530100fe4e12e
David Kim <soojongkim6@outlook.com>
2025-05-25 11:46:24 -0600

    Added Experience, Skills and Intro

---
+++ src/pages/Experience.tsx
@@ -0,0 +1,9 @@
+const Experience = () => {
+    return (
+        <div>
+            <h1>Expereicne</h1>
+        </div>
+    );
+};
+
+export default Experience;
---
+++ src/pages/Intro.tsx
@@ -0,0 +1,10 @@
+const Intro = () => {
+    return (
+        <section id="intro">
+            <h1>Hello! I am David Kim</h1>
+            <p>GIS Developer with a Passion for Cloud & Systems Architecture </p>
+        </section>
+    );
+};
+
+export default Intro;
